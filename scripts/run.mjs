869743a6a60d73a24887ca6aed22e987568b925c
Yi Huang <yi@secondstate.io>
2023-04-18 14:37:21 +0800

    Prepare data for comparison
    Signed-off-by: Yi Huang <yi@secondstate.io>

--- scripts/run.mjs
+++ scripts/run.mjs
@@ -4,13 +4,32 @@ const runner = argv._[0]
 const n_times = $.env.REPEAT || 1
 
 const files = await fs.readdir(dirname)
-let result = {};
-for (const f of files.filter(f => f.endsWith(".wasm"))) {
-    let sum = 0n;
-    for (let i = 0; i < n_times; i++) {
+    .then(files => files.filter(f => f.endsWith(".wasm")))
+
+let results = Object.fromEntries(files
+    .map(f => [f, { avg: 0n, var: 0n }]))
+
+for (let i = 0; i < n_times; i++) {
+    for (const f of files) {
         let { stdout } = await $`${runner} ${path.join(__dirname, dirname, f)}`
-        sum += BigInt(stdout)
+        let value = BigInt(stdout)
+        results[f].avg += value
+        results[f].var += value * value
     }
-    result[f] = (sum / BigInt(n_times)).toString()
 }
-console.log(JSON.stringify({ [dirname]: result }))
+
+for (let f of Object.keys(results)) {
+    results[f].avg = Number(results[f].avg / BigInt(n_times))
+    let var_sqr = ((n_times == 1) ?
+        0n :
+        (results[f].var / BigInt(n_times - 1))
+    )
+    results[f].var = Number(var_sqr) ** 0.5
+}
+
+console.log(JSON.stringify({
+    [dirname]: {
+        repeat: n_times,
+        result: results,
+    }
+}))
